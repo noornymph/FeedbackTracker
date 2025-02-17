324418cce7ff48fd065426dffd0860c8bf9e2bda
Khadija <khadijaaranoor2002@gmail.com>
2025-02-17 12:49:20 +0500

    feat: Views and serializer added

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Feedback
+from .serializers import FeedbackSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class FeedbackViewSet(viewsets.ModelViewSet):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    permission_classes = [IsAuthenticated]
